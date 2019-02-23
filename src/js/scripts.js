c05e0b89188a1e8e5c7306453e76edb3bf197497
ThomasBalaban <balabaninteractive@gmail.com>
2019-02-23 18:02:54 -0500

    aye

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,33 @@
-var moreInfo = document.getElementsByClassName('more-info');
+var moreInfo = document.getElementsByClassName('open-button');
+var closeBtn = document.getElementsByClassName('close-button');
+
+console.log(moreInfo);
 
 for (var i = 0; i < moreInfo.length; i++) {
     moreInfo[i].addEventListener('click', function(){
-        this.style.display = "none";
-        this.nextElementSibling.style.display = "block";
-    })
+        var info = this.parentNode.parentNode.getElementsByClassName('information-drop')[0];
+        //console.log(this.parentNode);
+
+        if(info.style.display == "block"){
+            info.style.display = "none";
+            this.parentNode.getElementsByClassName('close-button')[0].style.display = "none"; 
+        } else {
+            info.style.display = "block";
+            this.style.display = "none"; 
+            this.parentNode.getElementsByClassName('close-button')[0].style.display = "block"; 
+        }
+    });
+
+    closeBtn[i].addEventListener('click', function(){
+        //console.log(this.parentNode.parentNode.getElementsByClassName('image')[0].getElementsByClassName('information-drop')[0].style.display == "block");
+        if(this.parentNode.parentNode.getElementsByClassName('image')[0].getElementsByClassName('information-drop')[0].style.display == "block"){
+            this.parentNode.style.display = "flex";
+            this.parentNode.getElementsByClassName('open-button')[0].style.display = "block";
+            this.parentNode.parentNode.getElementsByClassName('image')[0].getElementsByClassName('information-drop')[0].style.display = "none";
+            this.style.display = "none"; 
+            
+        }
+    });
+
+    // I know there is a easier way to do this but short amount of time + fire ball = bad code // TO-DO: CLEAN THIS
 }
